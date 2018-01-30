5434492a4e16a05d0e5becc71599f24ca4aa6265
Ara <ayaghsizian@gmail.com>
2018-01-29 19:08:34 -0500

    seeing the html ok, refactored into GetHtmlAsync function

--- EbayScraperConsole/Program.cs
+++ EbayScraperConsole/Program.cs
@@ -13,12 +13,16 @@ namespace EbayScraperConsole
     {
         static void Main(string[] args)
         {
+            GetHtmlAsync();
+            Console.ReadLine(); // to slow the world down
+        }
+
+        public static void GetHtmlAsync()
+        {
             var url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1311.R1.TR12.TRC2.A0.H0.Xxbox.TRS0&_nkw=xbox+one&_sacat=0";
             var httpClient = new HttpClient();
             var html = httpClient.GetStringAsync(url);
-
             Console.WriteLine(html.Result);
-            Console.ReadLine(); // to slow the world down
         }
     }
 }
