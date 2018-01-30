f790cb817add3e175c512c299bbe038319ac3bd6
Ara <ayaghsizian@gmail.com>
2018-01-29 19:42:00 -0500

    ProductList count == 1 so we need to do some tweaking tomorrow

--- EbayScraperConsole/Program.cs
+++ EbayScraperConsole/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using HtmlAgilityPack;
 
 namespace EbayScraperConsole
 {
@@ -17,12 +18,24 @@ namespace EbayScraperConsole
             Console.ReadLine(); // to slow the world down
         }
 
-        public static void GetHtmlAsync()
+        // Commit Note: added keywords async and await
+        public static async void GetHtmlAsync()
         {
             var url = "https://www.ebay.com/sch/i.html?_from=R40&_trksid=p2380057.m570.l1311.R1.TR12.TRC2.A0.H0.Xxbox.TRS0&_nkw=xbox+one&_sacat=0";
             var httpClient = new HttpClient();
-            var html = httpClient.GetStringAsync(url);
-            Console.WriteLine(html.Result);
+            var html = await httpClient.GetStringAsync(url);
+
+            var htmlDocument = new HtmlDocument();
+
+            // Commit Note: added keywords async and await for this to work
+            htmlDocument.LoadHtml(html);
+
+            var ProductList = htmlDocument.DocumentNode.Descendants("ul")
+                .Where(node => node.GetAttributeValue("id", "").Equals("ListViewInner"))
+                .ToList();
+
+            Console.WriteLine(ProductList);
+
         }
     }
 }
